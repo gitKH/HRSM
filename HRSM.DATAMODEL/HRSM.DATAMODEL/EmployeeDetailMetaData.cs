b69e57fd95afe6323c8bf056543b4d4c3f6be197
VM-HOME <stathis@chrysikos.gr>
2016-05-20 21:26:13 +0300

    Changes Extract abstraction Employee Form from Index and Create views and put it in seperate file EmployeeForm as partial view

---
+++ HRSM.DATAMODEL/HRSM.DATAMODEL/EmployeeDetailMetaData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSM.DATAMODEL
+{
+    public class EmployeeDetailMetaData
+    {
+        [Display(Name = "ΑΤ")]
+        public string AT { get; set; }
+        [Display(Name = "ΑΦΜ")]
+        public string AFM { get; set; }
+        [Display(Name = "Φύλο")]
+        public Nullable<Gender> GENDER { get; set; }
+        [Display(Name = "Ημερομηνία Γέννησης")]
+        public Nullable<System.DateTime> BIRTHDATE { get; set; }
+        [Display(Name = "Έγγαμος")]
+        public Nullable<Marital> MARITALSTATUS { get; set; }
+        [Display(Name = "Ημερομηνία Λήξης Αδείας")]
+        public Nullable<System.DateTime> SECLICEXPDATE { get; set; }
+    }
+}
