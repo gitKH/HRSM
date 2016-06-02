4542d5a58903ddc9b8324eff2bd0c7c22ff5d514
Efstathios Chrysikos <stathis@chrysikos.gr>
2016-06-02 15:10:46 +0300

    Case Study

---
+++ CASE STUDIES/PassIEnumerableFromViewToController/PassIEnumerableFromViewToController/Controllers/ScheduleController.cs
@@ -0,0 +1,27 @@
+﻿using PassIEnumerableFromViewToController.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PassIEnumerableFromViewToController.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        //
+        // GET: /Schedule/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IEnumerable<DailySchedule> data)
+        {
+            return RedirectToAction("Index"); 
+        }
+
+    }
+}
---
+++ CASE STUDIES/PassIEnumerableFromViewToController/PassIEnumerableFromViewToController/Models/DailySchedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PassIEnumerableFromViewToController.Models
+{
+    public class DailySchedule
+    {
+        public string EMID { get; set; }
+        public string GSID { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+
+    }
+}
