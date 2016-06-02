4d09b80e65178ea9a30080a03edb4d2ae9a0a514
Efstathios Chrysikos <stathis@chrysikos.gr>
2016-06-02 13:41:40 +0300

    Case Study AutoComplete JqueryUI

---
+++ CASE STUDIES/AutoCompleteJqueryUI/AutoCompleteJqueryUI/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using AutoCompleteJqueryUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoCompleteJqueryUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Emp(string term)
+        {
+            EmployeesRepo data = new EmployeesRepo();
+
+            var result = data.Query().Where(x => x.Contains(term)).ToList();
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ CASE STUDIES/AutoCompleteJqueryUI/AutoCompleteJqueryUI/Models/EmployeesRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoCompleteJqueryUI.Models
+{
+    public class EmployeesRepo
+    {
+        public List<string> Query()
+        {
+            return new List<string>()
+            {
+                "stathis",
+                "giannis",
+                "kostas",
+                "dimitris",
+                "chris",
+                "vassiliki",
+                "thanasis",
+                "eleni",
+                "gamata"
+            };
+        }
+    }
+}
