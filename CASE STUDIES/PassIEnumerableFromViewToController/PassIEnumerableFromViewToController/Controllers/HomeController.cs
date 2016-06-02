c16d7f29feb1d753cd884e3fa3e015daca4fc570
Efstathios Chrysikos <stathis@chrysikos.gr>
2016-06-02 14:50:13 +0300

    Case Study Pas IEnumerable from View To Controller

---
+++ CASE STUDIES/PassIEnumerableFromViewToController/PassIEnumerableFromViewToController/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PassIEnumerableFromViewToController.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IEnumerable<string> data)
+        {
+            List<string> strs = new List<string>();
+            foreach (var item in data)
+            {
+                strs.Add(item);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ CASE STUDIES/PassIEnumerableFromViewToController/PassIEnumerableFromViewToController/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PassIEnumerableFromViewToController.MvcApplication" Language="C#" %>
